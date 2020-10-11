3e8d36244523ebbb00a0b77496851c929c83e411
rtazaki <rtazaki@gmail.com>
2020-10-11 21:11:17 +0900

    駒の働きと選択を実装。(要リファクタリング)
    [動作検討]
    player1.movePos.isNotEmpty()で選択クリアを入れると駒範囲選択と、駒移動が完全に分離できる。
    これを行うと、例えば金を押して、直後に玉を押したときに、玉の範囲選択にならずに一旦範囲外でクリアされる挙動となる。
    
    現在の実装でも、さして違和感がないため、今回はこのまま進めることとする。

--- app/src/main/java/io/github/rtazaki/kyotoshogi/MainActivity.kt
+++ app/src/main/java/io/github/rtazaki/kyotoshogi/MainActivity.kt
@@ -21,60 +21,60 @@ class MainActivity : AppCompatActivity() {
      */
     private val mapPtoB by lazy {
         mapOf(
-            Player.Pos(1, 1) to binding.board11,
-            Player.Pos(1, 2) to binding.board12,
-            Player.Pos(1, 3) to binding.board13,
-            Player.Pos(1, 4) to binding.board14,
-            Player.Pos(1, 5) to binding.board15,
-            Player.Pos(2, 1) to binding.board21,
-            Player.Pos(2, 2) to binding.board22,
-            Player.Pos(2, 3) to binding.board23,
-            Player.Pos(2, 4) to binding.board24,
-            Player.Pos(2, 5) to binding.board25,
-            Player.Pos(3, 1) to binding.board31,
-            Player.Pos(3, 2) to binding.board32,
-            Player.Pos(3, 3) to binding.board33,
-            Player.Pos(3, 4) to binding.board34,
-            Player.Pos(3, 5) to binding.board35,
-            Player.Pos(4, 1) to binding.board41,
-            Player.Pos(4, 2) to binding.board42,
-            Player.Pos(4, 3) to binding.board43,
-            Player.Pos(4, 4) to binding.board44,
-            Player.Pos(4, 5) to binding.board45,
-            Player.Pos(5, 1) to binding.board51,
-            Player.Pos(5, 2) to binding.board52,
-            Player.Pos(5, 3) to binding.board53,
-            Player.Pos(5, 4) to binding.board54,
-            Player.Pos(5, 5) to binding.board55,
+            FieldPiece.Pos(1, 1) to binding.board11,
+            FieldPiece.Pos(1, 2) to binding.board12,
+            FieldPiece.Pos(1, 3) to binding.board13,
+            FieldPiece.Pos(1, 4) to binding.board14,
+            FieldPiece.Pos(1, 5) to binding.board15,
+            FieldPiece.Pos(2, 1) to binding.board21,
+            FieldPiece.Pos(2, 2) to binding.board22,
+            FieldPiece.Pos(2, 3) to binding.board23,
+            FieldPiece.Pos(2, 4) to binding.board24,
+            FieldPiece.Pos(2, 5) to binding.board25,
+            FieldPiece.Pos(3, 1) to binding.board31,
+            FieldPiece.Pos(3, 2) to binding.board32,
+            FieldPiece.Pos(3, 3) to binding.board33,
+            FieldPiece.Pos(3, 4) to binding.board34,
+            FieldPiece.Pos(3, 5) to binding.board35,
+            FieldPiece.Pos(4, 1) to binding.board41,
+            FieldPiece.Pos(4, 2) to binding.board42,
+            FieldPiece.Pos(4, 3) to binding.board43,
+            FieldPiece.Pos(4, 4) to binding.board44,
+            FieldPiece.Pos(4, 5) to binding.board45,
+            FieldPiece.Pos(5, 1) to binding.board51,
+            FieldPiece.Pos(5, 2) to binding.board52,
+            FieldPiece.Pos(5, 3) to binding.board53,
+            FieldPiece.Pos(5, 4) to binding.board54,
+            FieldPiece.Pos(5, 5) to binding.board55,
         )
     }
     private val mapBtoP by lazy {
         mapOf(
-            binding.board11 to Player.Pos(1, 1),
-            binding.board12 to Player.Pos(1, 2),
-            binding.board13 to Player.Pos(1, 3),
-            binding.board14 to Player.Pos(1, 4),
-            binding.board15 to Player.Pos(1, 5),
-            binding.board21 to Player.Pos(2, 1),
-            binding.board22 to Player.Pos(2, 2),
-            binding.board23 to Player.Pos(2, 3),
-            binding.board24 to Player.Pos(2, 4),
-            binding.board25 to Player.Pos(2, 5),
-            binding.board31 to Player.Pos(3, 1),
-            binding.board32 to Player.Pos(3, 2),
-            binding.board33 to Player.Pos(3, 3),
-            binding.board34 to Player.Pos(3, 4),
-            binding.board35 to Player.Pos(3, 5),
-            binding.board41 to Player.Pos(4, 1),
-            binding.board42 to Player.Pos(4, 2),
-            binding.board43 to Player.Pos(4, 3),
-            binding.board44 to Player.Pos(4, 4),
-            binding.board45 to Player.Pos(4, 5),
-            binding.board51 to Player.Pos(5, 1),
-            binding.board52 to Player.Pos(5, 2),
-            binding.board53 to Player.Pos(5, 3),
-            binding.board54 to Player.Pos(5, 4),
-            binding.board55 to Player.Pos(5, 5),
+            binding.board11 to FieldPiece.Pos(1, 1),
+            binding.board12 to FieldPiece.Pos(1, 2),
+            binding.board13 to FieldPiece.Pos(1, 3),
+            binding.board14 to FieldPiece.Pos(1, 4),
+            binding.board15 to FieldPiece.Pos(1, 5),
+            binding.board21 to FieldPiece.Pos(2, 1),
+            binding.board22 to FieldPiece.Pos(2, 2),
+            binding.board23 to FieldPiece.Pos(2, 3),
+            binding.board24 to FieldPiece.Pos(2, 4),
+            binding.board25 to FieldPiece.Pos(2, 5),
+            binding.board31 to FieldPiece.Pos(3, 1),
+            binding.board32 to FieldPiece.Pos(3, 2),
+            binding.board33 to FieldPiece.Pos(3, 3),
+            binding.board34 to FieldPiece.Pos(3, 4),
+            binding.board35 to FieldPiece.Pos(3, 5),
+            binding.board41 to FieldPiece.Pos(4, 1),
+            binding.board42 to FieldPiece.Pos(4, 2),
+            binding.board43 to FieldPiece.Pos(4, 3),
+            binding.board44 to FieldPiece.Pos(4, 4),
+            binding.board45 to FieldPiece.Pos(4, 5),
+            binding.board51 to FieldPiece.Pos(5, 1),
+            binding.board52 to FieldPiece.Pos(5, 2),
+            binding.board53 to FieldPiece.Pos(5, 3),
+            binding.board54 to FieldPiece.Pos(5, 4),
+            binding.board55 to FieldPiece.Pos(5, 5),
         )
     }
 
@@ -88,30 +88,59 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         // 初期配置
         val player1 = Player()
-        for (p in player1.getOnBoards()) {
+        player1.piecePos.forEach { p ->
             mapPtoB.getValue(p.boardPos).text = p.onBoards
         }
         val player2 = Player()
-        for (p in player2.getOnBoards()) {
-            val mirrorPos = player2.getMirrorPos(p.boardPos)
+        player2.piecePos.forEach { p ->
+            val mirrorPos = FieldPiece.getMirrorPos(p.boardPos)
             mapPtoB.getValue(mirrorPos).text = p.onBoards
             mapPtoB.getValue(mirrorPos).rotation = 180.0F
         }
-        for (b in mapBtoP.keys) {
+        mapBtoP.keys.forEach { b ->
             b.setOnClickListener {
                 if (turn) {
-                    for (p in player1.getOnBoards()) {
-                        if (mapBtoP.getValue(b) == p.boardPos) {
-                            Log.d("駒▲", "${b.text}")
+                    player1.movePos.forEach { m ->
+                        if (m == mapBtoP.getValue(b)) {
+                            Log.d("駒", "▲→△")
                             turn = false
+                        }
+                        mapBtoP.keys.forEach { it.setBackgroundResource(R.drawable.button_background) }
+                    }
+                    player1.movePos.clear()
+                    player1.piecePos.forEach { p ->
+                        if (p.boardPos == mapBtoP.getValue(b)) {
+                            Log.d("駒", "▲ ${b.text}")
+                            player1.selectPos = p.boardPos
+                            player1.movePos =
+                                FieldPiece.getMovePos(p, player1, player2)
+                            Log.d("駒", "稼働範囲: ${player1.movePos}")
+                            player1.movePos.forEach { m ->
+                                mapPtoB.getValue(m)
+                                    .setBackgroundResource(R.drawable.button_background_move)
+                            }
                         }
                     }
                 } else {
-                    for (p in player2.getOnBoards()) {
-                        val mirrorPos = player2.getMirrorPos(p.boardPos)
-                        if (mapBtoP.getValue(b) == mirrorPos) {
-                            Log.d("駒△", "${b.text}")
+                    player2.movePos.forEach { m ->
+                        if (m == mapBtoP.getValue(b)) {
+                            Log.d("駒", "△→▲")
                             turn = true
+                        }
+                        mapBtoP.keys.forEach { it.setBackgroundResource(R.drawable.button_background) }
+                    }
+                    player2.piecePos.forEach { p ->
+                        val mirrorPos = FieldPiece.getMirrorPos(p.boardPos)
+                        if (mirrorPos == mapBtoP.getValue(b)) {
+                            Log.d("駒", "△ ${b.text}")
+                            player2.selectPos = mirrorPos
+                            player2.movePos =
+                                FieldPiece.getMovePos(p, player2, player1, mirror = true)
+                            Log.d("駒", "稼働範囲: ${player2.movePos}")
+                            player2.movePos.forEach { m ->
+                                mapPtoB.getValue(m)
+                                    .setBackgroundResource(R.drawable.button_background_move)
+                            }
                         }
                     }
                 }
--- app/src/main/java/io/github/rtazaki/kyotoshogi/Player.kt
+++ app/src/main/java/io/github/rtazaki/kyotoshogi/Player.kt
@@ -1,22 +1,16 @@
 package io.github.rtazaki.kyotoshogi
 
 class Player {
-    data class PiecePos(var onBoards: String, var boardPos: Pos)
-    data class Pos(val column: Int, val row: Int)
+    data class PiecePos(val onBoards: String, val boardPos: FieldPiece.Pos)
 
-    private var piecePos = mutableListOf(
-        PiecePos("と", Pos(5, 5)),
-        PiecePos("銀", Pos(4, 5)),
-        PiecePos("玉", Pos(3, 5)),
-        PiecePos("金", Pos(2, 5)),
-        PiecePos("歩", Pos(1, 5)),
+    var piecePos = mutableListOf(
+        PiecePos("と", FieldPiece.Pos(5, 5)),
+        PiecePos("銀", FieldPiece.Pos(4, 5)),
+        PiecePos("玉", FieldPiece.Pos(3, 5)),
+        PiecePos("金", FieldPiece.Pos(2, 5)),
+        PiecePos("歩", FieldPiece.Pos(1, 5)),
     )
-    private var hands = mutableListOf<String>()
-    fun getOnBoards(): MutableList<PiecePos> {
-        return piecePos
-    }
-
-    fun getMirrorPos(pos: Pos): Pos {
-        return Pos(6 - pos.column, 6 - pos.row)
-    }
+    var movePos = mutableListOf<FieldPiece.Pos>()
+    var selectPos = FieldPiece.Pos(0, 0)
+    var hands = mutableListOf<String>()
 }
