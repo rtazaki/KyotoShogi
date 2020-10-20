04b246f1c6516f575547a2b817f61ff7a0b3ef48
rtazaki <rtazaki@gmail.com>
2020-10-20 23:11:45 +0900

    玉のUnitTestを作成。

---
+++ app/src/test/java/io/github/rtazaki/kyotoshogi/GetMovePosUnitTest/KingUnitTest.kt
@@ -0,0 +1,308 @@
+package io.github.rtazaki.kyotoshogi.getMovePosUnitTest
+
+import io.github.rtazaki.kyotoshogi.MainGame
+import org.junit.Test
+
+/**
+ * 玉
+ */
+class KingUnitTest {
+    /**
+     * 通常系
+     */
+    @Test
+    fun getMovePosKing_1_Test() {
+        val player = mapOf(true to MainGame.Player(), false to MainGame.Player())
+        player.getValue(true).pieces[MainGame.Pos(3, 3)] = "玉"
+        val move = MainGame.getMovePos(
+            piece = mapOf(MainGame.Pos(3, 3) to "玉").entries.first(),
+            player.getValue(true),
+            player.getValue(false),
+            false
+        )
+        val t = listOf(
+            MainGame.Pos(4, 2),
+            MainGame.Pos(3, 2),
+            MainGame.Pos(2, 2),
+            MainGame.Pos(4, 3),
+            MainGame.Pos(2, 3),
+            MainGame.Pos(4, 4),
+            MainGame.Pos(3, 4),
+            MainGame.Pos(2, 4)
+        )
+        assert(move.containsAll(t))
+        assert(t.containsAll(move))
+    }
+
+    /**
+     * 境界(5筋)
+     */
+    @Test
+    fun getMovePosKing_2_Test() {
+        val player = mapOf(true to MainGame.Player(), false to MainGame.Player())
+        player.getValue(true).pieces[MainGame.Pos(5, 3)] = "玉"
+        val move = MainGame.getMovePos(
+            piece = mapOf(MainGame.Pos(5, 3) to "玉").entries.first(),
+            player.getValue(true),
+            player.getValue(false),
+            false
+        )
+        val t = listOf(
+            MainGame.Pos(5, 2),
+            MainGame.Pos(4, 2),
+            MainGame.Pos(4, 3),
+            MainGame.Pos(5, 4),
+            MainGame.Pos(4, 4)
+        )
+        assert(move.containsAll(t))
+        assert(t.containsAll(move))
+    }
+
+    /**
+     * 境界(1筋)
+     */
+    @Test
+    fun getMovePosKing_3_Test() {
+        val player = mapOf(true to MainGame.Player(), false to MainGame.Player())
+        player.getValue(true).pieces[MainGame.Pos(1, 3)] = "玉"
+        val move = MainGame.getMovePos(
+            piece = mapOf(MainGame.Pos(1, 3) to "玉").entries.first(),
+            player.getValue(true),
+            player.getValue(false),
+            false
+        )
+        val t = listOf(
+            MainGame.Pos(2, 2),
+            MainGame.Pos(1, 2),
+            MainGame.Pos(2, 3),
+            MainGame.Pos(2, 4),
+            MainGame.Pos(1, 4)
+        )
+        assert(move.containsAll(t))
+        assert(t.containsAll(move))
+    }
+
+    /**
+     * 境界(5段)
+     */
+    @Test
+    fun getMovePosKing_4_Test() {
+        val player = mapOf(true to MainGame.Player(), false to MainGame.Player())
+        player.getValue(true).pieces[MainGame.Pos(3, 5)] = "玉"
+        val move = MainGame.getMovePos(
+            piece = mapOf(MainGame.Pos(3, 5) to "玉").entries.first(),
+            player.getValue(true),
+            player.getValue(false),
+            false
+        )
+        val t = listOf(
+            MainGame.Pos(4, 4),
+            MainGame.Pos(3, 4),
+            MainGame.Pos(2, 4),
+            MainGame.Pos(4, 5),
+            MainGame.Pos(2, 5)
+        )
+        assert(move.containsAll(t))
+        assert(t.containsAll(move))
+    }
+
+    /**
+     * 境界(1段)
+     */
+    @Test
+    fun getMovePosKing_5_Test() {
+        val player = mapOf(true to MainGame.Player(), false to MainGame.Player())
+        player.getValue(true).pieces[MainGame.Pos(3, 1)] = "玉"
+        val move = MainGame.getMovePos(
+            piece = mapOf(MainGame.Pos(3, 1) to "玉").entries.first(),
+            player.getValue(true),
+            player.getValue(false),
+            false
+        )
+        val t = listOf(
+            MainGame.Pos(4, 1),
+            MainGame.Pos(2, 1),
+            MainGame.Pos(4, 2),
+            MainGame.Pos(3, 2),
+            MainGame.Pos(2, 2)
+        )
+        assert(move.containsAll(t))
+        assert(t.containsAll(move))
+    }
+
+    /**
+     * 移動先に自駒がいたら範囲外
+     */
+    @Test
+    fun getMovePosKing_6_Test() {
+        val player = mapOf(true to MainGame.Player(), false to MainGame.Player())
+        player.getValue(true).pieces[MainGame.Pos(3, 3)] = "玉"
+        player.getValue(true).pieces[MainGame.Pos(4, 2)] = "歩"
+        player.getValue(true).pieces[MainGame.Pos(3, 2)] = "歩"
+        player.getValue(true).pieces[MainGame.Pos(2, 2)] = "歩"
+        player.getValue(true).pieces[MainGame.Pos(4, 3)] = "歩"
+        player.getValue(true).pieces[MainGame.Pos(2, 3)] = "歩"
+        player.getValue(true).pieces[MainGame.Pos(4, 4)] = "歩"
+        player.getValue(true).pieces[MainGame.Pos(3, 4)] = "歩"
+        player.getValue(true).pieces[MainGame.Pos(2, 4)] = "歩"
+        val move = MainGame.getMovePos(
+            piece = mapOf(MainGame.Pos(3, 3) to "玉").entries.first(),
+            player.getValue(true),
+            player.getValue(false),
+            false
+        )
+        val t = listOf<MainGame.Pos>()
+        assert(move.containsAll(t))
+        assert(t.containsAll(move))
+    }
+
+    /**
+     * 後手_通常系
+     */
+    @Test
+    fun getMovePosKing_7_Test() {
+        val player = mapOf(true to MainGame.Player(), false to MainGame.Player())
+        player.getValue(false).pieces[MainGame.Pos(3, 3)] = "玉"
+        val move = MainGame.getMovePos(
+            piece = mapOf(MainGame.Pos(3, 3) to "玉").entries.first(),
+            player.getValue(false),
+            player.getValue(true),
+            true
+        )
+        val t = listOf(
+            MainGame.Pos(2, 4),
+            MainGame.Pos(3, 4),
+            MainGame.Pos(4, 4),
+            MainGame.Pos(2, 3),
+            MainGame.Pos(4, 3),
+            MainGame.Pos(2, 2),
+            MainGame.Pos(3, 2),
+            MainGame.Pos(4, 2)
+        )
+        assert(move.containsAll(t))
+        assert(t.containsAll(move))
+    }
+
+    /**
+     * 後手_境界(5筋)
+     */
+    @Test
+    fun getMovePosKing_8_Test() {
+        val player = mapOf(true to MainGame.Player(), false to MainGame.Player())
+        player.getValue(false).pieces[MainGame.Pos(5, 3)] = "玉"
+        val move = MainGame.getMovePos(
+            piece = mapOf(MainGame.Pos(5, 3) to "玉").entries.first(),
+            player.getValue(false),
+            player.getValue(true),
+            true
+        )
+        val t = listOf(
+            MainGame.Pos(1, 4),
+            MainGame.Pos(2, 4),
+            MainGame.Pos(2, 3),
+            MainGame.Pos(1, 2),
+            MainGame.Pos(2, 2)
+        )
+        assert(move.containsAll(t))
+        assert(t.containsAll(move))
+    }
+
+    /**
+     * 後手_境界(1筋)
+     */
+    @Test
+    fun getMovePosKing_9_Test() {
+        val player = mapOf(true to MainGame.Player(), false to MainGame.Player())
+        player.getValue(false).pieces[MainGame.Pos(1, 3)] = "玉"
+        val move = MainGame.getMovePos(
+            piece = mapOf(MainGame.Pos(1, 3) to "玉").entries.first(),
+            player.getValue(false),
+            player.getValue(true),
+            true
+        )
+        val t = listOf(
+            MainGame.Pos(4, 4),
+            MainGame.Pos(5, 4),
+            MainGame.Pos(4, 3),
+            MainGame.Pos(4, 2),
+            MainGame.Pos(5, 2)
+        )
+        assert(move.containsAll(t))
+        assert(t.containsAll(move))
+    }
+
+    /**
+     * 後手_境界(5段)
+     */
+    @Test
+    fun getMovePosKing_10_Test() {
+        val player = mapOf(true to MainGame.Player(), false to MainGame.Player())
+        player.getValue(false).pieces[MainGame.Pos(3, 5)] = "玉"
+        val move = MainGame.getMovePos(
+            piece = mapOf(MainGame.Pos(3, 5) to "玉").entries.first(),
+            player.getValue(false),
+            player.getValue(true),
+            true
+        )
+        val t = listOf(
+            MainGame.Pos(2, 2),
+            MainGame.Pos(3, 2),
+            MainGame.Pos(4, 2),
+            MainGame.Pos(2, 1),
+            MainGame.Pos(4, 1)
+        )
+        assert(move.containsAll(t))
+        assert(t.containsAll(move))
+    }
+
+    /**
+     * 後手_境界(1段)
+     */
+    @Test
+    fun getMovePosKing_11_Test() {
+        val player = mapOf(true to MainGame.Player(), false to MainGame.Player())
+        player.getValue(false).pieces[MainGame.Pos(3, 1)] = "玉"
+        val move = MainGame.getMovePos(
+            piece = mapOf(MainGame.Pos(3, 1) to "玉").entries.first(),
+            player.getValue(false),
+            player.getValue(true),
+            true
+        )
+        val t = listOf(
+            MainGame.Pos(2, 5),
+            MainGame.Pos(4, 5),
+            MainGame.Pos(2, 4),
+            MainGame.Pos(3, 4),
+            MainGame.Pos(4, 4)
+        )
+        assert(move.containsAll(t))
+        assert(t.containsAll(move))
+    }
+
+    /**
+     * 後手_移動先に自駒がいたら範囲外
+     */
+    @Test
+    fun getMovePosKing_12_Test() {
+        val player = mapOf(true to MainGame.Player(), false to MainGame.Player())
+        player.getValue(false).pieces[MainGame.Pos(3, 3)] = "玉"
+        player.getValue(false).pieces[MainGame.Pos(4, 2)] = "歩"
+        player.getValue(false).pieces[MainGame.Pos(3, 2)] = "歩"
+        player.getValue(false).pieces[MainGame.Pos(2, 2)] = "歩"
+        player.getValue(false).pieces[MainGame.Pos(4, 3)] = "歩"
+        player.getValue(false).pieces[MainGame.Pos(2, 3)] = "歩"
+        player.getValue(false).pieces[MainGame.Pos(4, 4)] = "歩"
+        player.getValue(false).pieces[MainGame.Pos(3, 4)] = "歩"
+        player.getValue(false).pieces[MainGame.Pos(2, 4)] = "歩"
+        val move = MainGame.getMovePos(
+            piece = mapOf(MainGame.Pos(3, 3) to "玉").entries.first(),
+            player.getValue(false),
+            player.getValue(true),
+            true
+        )
+        val t = listOf<MainGame.Pos>()
+        assert(move.containsAll(t))
+        assert(t.containsAll(move))
+    }
+
+}
